257b06aaba6aa4b857603703d7480e92419d73a1
David A. Ventimiglia <davidaventimiglia@hasura.io>
2024-08-02 11:12:02 -0700

    Trying a simple Java HTTP Server

--- src/main/java/com/hasura/App.java
+++ src/main/java/com/hasura/App.java
@@ -1,13 +1,23 @@
 package com.hasura;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.*;
+import javax.xml.ws.*;
+import javax.xml.ws.http.*;
+import javax.xml.transform.*;
+import javax.xml.transform.stream.*;
+
+@WebServiceProvider
+@ServiceMode(value = Service.Mode.PAYLOAD)
+public class App implements Provider<Source> {
+    @Override
+    public Source invoke(Source request) {
+	return new StreamSource(new StringReader("<p>Hello There!</p>"));
+    }
+    public static void main (String[] args) throws InterruptedException {
+	String address = "http://127.0.0.1:8000/";
+	Endpoint.create(HTTPBinding.HTTP_BINDING, new App()).publish(address);
+	System.out.println("Service running at " + address);
+	System.out.println("Type [CTRL]+[C] to quit!");
+	Thread.sleep(Long.MAX_VALUE);
     }
 }
