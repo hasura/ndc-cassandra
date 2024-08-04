d5199b1cef01c46a31b60db512a619e626bae6fa
David A. Ventimiglia <davidaventimiglia@hasura.io>
2024-08-04 16:23:41 -0700

    Simplified

--- src/main/java/com/hasura/App.java
+++ src/main/java/com/hasura/App.java
@@ -3,17 +3,20 @@ package com.hasura;
 import com.sun.net.httpserver.*;
 import java.io.*;
 import java.net.*;
+import java.util.concurrent.Executors;
+import java.util.*;
     
 class App implements HttpHandler {
-    public static void main (String[] args) throws IOException {
+	public static void main (String[] args) throws IOException {
 	HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+	server.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
 	server.createContext("/applications/myapp", new App());
-	server.setExecutor(null); // creates a default executor
+	server.setExecutor(null);
 	server.start();
     }
 
-    public void handle(HttpExchange t) throws IOException {
-	InputStream is = t.getRequestBody();
+	public void handle(HttpExchange t) throws IOException {
+	// InputStream is = t.getRequestBody();
 	String response = "This is the response";
 	t.sendResponseHeaders(200, response.length());
 	OutputStream os = t.getResponseBody();
