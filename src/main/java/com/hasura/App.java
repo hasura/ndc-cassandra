e46072d1c7568746fdf3043a6258c56b3ae7917f
David A. Ventimiglia <davidaventimiglia@hasura.io>
2024-08-05 11:42:51 -0700

    Added the healthz and stubbed out the metrics endpoints

--- src/main/java/com/hasura/App.java
+++ src/main/java/com/hasura/App.java
@@ -10,10 +10,36 @@ class App {
     public static void main (String[] args) throws IOException {
 	var response = "This is the response".getBytes();
 	HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-	server.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
-	server.createContext("/", exchange -> {
-		exchange.sendResponseHeaders(200, response.length);
-		try (var os = exchange.getResponseBody()) {
-		    os.write(response);}});
+	server
+	    .setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+	server
+	    .createContext("/", exchange -> {
+		    exchange.sendResponseHeaders(200, response.length);
+		    try (var os = exchange.getResponseBody()) {
+			os.write(response);}});
+	server
+	    .createContext("/healthz",
+			   exchange -> {
+			       exchange
+				   .sendResponseHeaders(204, 0);});
+	server
+	    .createContext("/metrics",
+			   exchange -> {
+			       exchange
+				   .getResponseHeaders()
+				   .add("content-type", "text/plain");
+			       exchange
+				   .sendResponseHeaders(200, 0);
+			       exchange
+				   .getResponseBody()
+				   .write("""
+# HELP active_requests number of active requests
+# TYPE active_requests gauge
+active_requests 1
+# HELP total_requests number of total requests
+# TYPE total_requests counter
+total_requests 48
+					    """
+					    .getBytes());});
 	server.start();}}
 
